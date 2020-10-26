95dfd7bc77a19646288a751ded9352e4735936b6
畏醉潜逃 <eric@im1k.net>
2020-10-26 16:04:45 +0800

    迁移服务器

--- config/index.js
+++ config/index.js
@@ -12,9 +12,9 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/apis': {
-         target: 'http://localhost:8078/apis/', // 接口域名
+        //  target: 'http://localhost:8078/apis/', // 接口域名
         // target: 'http://meal-admin.food-edu.net/apis/', // 接口域名
-        //target: 'http://admin.huomei.youshan.co/apis/', // 新接口域名
+        target: 'http://admin.huomei.youshan.co/apis/', // 新接口域名
         ws: true,
         changeOrigin: false, //是否跨域
         pathRewrite: {
